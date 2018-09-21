2c2598da7a61e72ea5d9141f77cbb7745b8ea346
Michael Lyon <lyoncodes@gmail.com>
2018-09-21 09:28:48 -0700

    solved for multiply

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b) { 
     var output = [(a + b), "The sum of 4 and 7 is 11."];
     return output
-};
+    };
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -49,11 +49,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+var output = [(a,b,c)), (a,b,c)), "4 and 7 and 5 sum to 16.", "The product of 4 and 7 and 5 is 140."]
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
